a123a735d88a786b9128b68ed2734b105b07d72b
Stekolschikov <stekolschikov@mail.ua>
2017-04-19 16:05:34 +0300

    Меню, позвонить, сайт, выходи

--- AU/MainActivity.cs
+++ AU/MainActivity.cs
@@ -3,6 +3,8 @@ using Android.Widget;
 using Android.OS;
 using Android.Content;
 using Android.Graphics.Drawables;
+using Android.Telephony;
+using Android.Views;
 
 namespace AU
 {
@@ -15,6 +17,10 @@ namespace AU
         Intent Ddata;
         Result code;
         int Rcode;
+
+        private const int IDM_CALL = 1;
+        private const int IDM_SITE = 2;
+        private const int IDM_CLOSE = 3;
 
         protected override void OnCreate(Bundle bundle)
         {
@@ -32,6 +38,8 @@ namespace AU
             var actionBar = this.ActionBar;
             actionBar.SetBackgroundDrawable(new ColorDrawable(Android.Graphics.Color.ParseColor("#981b07")));
 
+            phone.Text = GetMyPhoneNumber();
+
 
             button.Click += delegate {
                 Intent = new Intent();
@@ -40,17 +48,17 @@ namespace AU
                 StartActivityForResult(Intent.CreateChooser(Intent, "Select Picture"), 99);
             };
 
+            // клик на нижнюю кнопку
             btnColl.Click += delegate {
                 var mailIntent = new Intent(Intent.ActionSend);
                 mailIntent.SetType("message/rfc822");
-                mailIntent.PutExtra(Intent.ExtraEmail, new string[] { "mou.mail.com@gmail.com" });
+                mailIntent.PutExtra(Intent.ExtraEmail, new string[] { "info@artultra.ru" });
                 mailIntent.PutExtra(Intent.ExtraSubject, "заказ из приложения");
-                mailIntent.PutExtra(Intent.ExtraText, name.Text + " --- " + phone.Text + " --- " + date.Text);
+                mailIntent.PutExtra(Intent.ExtraText, "Имя: " + name.Text + "; " + "Телефон: " + phone.Text + "; " + "Дата: " + date.Text);
                 if (Rcode == 99)
                     mailIntent.PutExtra(Intent.ExtraStream, Ddata.Data);
-
                 try { StartActivity(mailIntent); popup("Письмо переданно на отправку!"); }
-                catch { popup("настройте почту на вашем устройстве"); }
+                catch { popup("Настройте почту на вашем устройстве"); }
 
             };
 
@@ -65,6 +73,55 @@ namespace AU
                 popup("Каратинка выбра!");
         }
 
+
+        [assembly: Xamarin.Forms.Dependency(typeof(PhoneNumberService))]
+        public string GetMyPhoneNumber()
+        {
+            TelephonyManager mgr = Application.Context.GetSystemService(Context.TelephonyService) as TelephonyManager;
+            return mgr.Line1Number;
+        }
+
+        //Определение создания меню
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            base.OnCreateOptionsMenu(menu);
+            int groupId = 0;
+
+            IMenuItem menuItem1 = menu.Add(groupId, IDM_CALL, Menu.None, "Позвонить");
+            IMenuItem menuItem2 = menu.Add(groupId, IDM_SITE, Menu.None, "Перейти на сайт");
+            IMenuItem menuItem3 = menu.Add(groupId, IDM_CLOSE, Menu.None, "Закрыть");
+
+            return true;
+        }
+        //Описание обработчика события выбора пункта меню
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            string selectedItem = string.Empty;
+
+            switch (item.ItemId)
+            {
+                case IDM_CALL:
+                    var uri = Android.Net.Uri.Parse("tel:+7(495)7291394");
+                    var intent = new Intent(Intent.ActionDial, uri);
+                    StartActivity(intent);
+                    break;
+                case IDM_SITE:
+                    var uriss = Android.Net.Uri.Parse("http://artultra.ru/");
+                    var intentt = new Intent(Intent.ActionView, uriss);
+                    StartActivity(intentt);
+                    break;
+                case IDM_CLOSE:
+                    Finish();
+                    selectedItem = "Close";
+                    break;
+                default:
+                    return false;
+            }
+           
+            return true;
+        }
+
+        // Сообщения
         public void popup(string s)
         {
             Toast.MakeText(this, s, ToastLength.Short).Show();
